59d0ed858e4d32ede5ba34a56fdde04ca4ed3dd0
HasanC14 <chowdhuryh999@gmail.com>
2024-06-17 00:42:16 +0600

    Tryed to optimize the server

--- Client/src/components/ProductSearch.jsx
+++ Client/src/components/ProductSearch.jsx
@@ -4,8 +4,10 @@ import StarTech from "../assets/startech.png";
 import Ryans from "../assets/rayans.svg";
 import PCHouse from "../assets/pchousew.webp";
 import TechLand from "../assets/techland.webp";
+import Ultra from "../assets/ultra.webp";
+import { FaCircleInfo } from "react-icons/fa6";
+
 const extractNumbersFromString = (str) => {
-  // Match digits, optional commas, and optional decimal point
   const regex = /[0-9]+(?:,[0-9]{3})*(?:\.[0-9]+)?/g;
   const matches = str.match(regex);
 
@@ -22,9 +24,21 @@ const ProductSearch = () => {
   const [inputValue, setInputValue] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [products, setProducts] = useState(null);
+  const [loadingGif, setLoadingGif] = useState("");
+
+  const gifUrls = [
+    "https://giphy.com/embed/rIpm4UI7pv7WFiSm4s",
+    "https://giphy.com/embed/ZXf6dCfo4h9br1PMEQ",
+    "https://giphy.com/embed/RIwWQuRrVJLC4AB5Ik",
+    "https://giphy.com/embed/ghbSg8kaE3U15aD2cp",
+    "https://giphy.com/embed/gIzLZvcPFQyeQd4oRY",
+    "https://giphy.com/embed/9itja4ux9fpFTSu7le ",
+    "https://giphy.com/embed/l2Rno9dUdSVxorDj2",
+  ];
 
   const handleSearch = async () => {
     setIsLoading(true);
+    setLoadingGif(gifUrls[Math.floor(Math.random() * gifUrls.length)]);
     try {
       const response = await fetch(
         `http://localhost:3000/scrape?product=${inputValue}`
@@ -40,15 +54,28 @@ const ProductSearch = () => {
 
   return (
     <div className="container mx-auto px-4 py-8">
-      {/* <img src={Binary} alt="" /> */}
+      <div className="flex text-red-500 items-center">
+        <FaCircleInfo className="mr-2" /> Please provide a accurate product name
+        for better results.
+      </div>
       <div className="flex items-center">
-        <input
-          type="text"
-          className="border border-gray-300 p-2 mr-2"
-          placeholder="Enter product name..."
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-        />
+        <div className="bg-white p-4 rounded-lg">
+          <div className="relative bg-inherit">
+            <input
+              type="text"
+              id="search"
+              name="search"
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+              className="peer bg-transparent h-10 w-72 rounded-lg text-gray-500 placeholder-transparent ring-2 px-2 ring-gray-500 focus:ring-sky-600 focus:outline-none focus:border-rose-600"
+              placeholder="Type inside me"
+            />
+
+            <label className="absolute cursor-text left-0 -top-3 text-sm text-gray-500 bg-inherit mx-1 px-1 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-placeholder-shown:top-2 peer-focus:-top-3 peer-focus:text-sky-600 peer-focus:text-sm transition-all">
+              Search
+            </label>
+          </div>
+        </div>
         <button
           className="bg-blue-500 text-white px-4 py-2 rounded"
           onClick={handleSearch}
@@ -58,38 +85,42 @@ const ProductSearch = () => {
         </button>
       </div>
 
-      {isLoading && <p className="text-gray-500 mt-4">Loading...</p>}
+      {isLoading && (
+        <div className="flex justify-center items-center mt-4">
+          <iframe
+            src={loadingGif}
+            width="480"
+            height="271"
+            style={{ border: "none" }}
+            frameBorder="0"
+            className="giphy-embed"
+            allowFullScreen
+            title="Loading"
+          ></iframe>
+        </div>
+      )}
 
       {products && (
         <div className="mt-8">
           {Object.keys(products).map((shop) => (
             <div key={shop} className="mb-8">
               {shop === "Binary" && (
-                <img src={Binary} alt={shop} className="size-10 rounded-full" />
+                <img src={Binary} alt={shop} className="w-20" />
               )}
               {shop === "StarTech" && (
-                <img
-                  src={StarTech}
-                  alt={shop}
-                  className="size-10 rounded-full"
-                />
+                <img src={StarTech} alt={shop} className="w-20" />
               )}
               {shop === "Ryans" && (
-                <img src={Ryans} alt={shop} className="size-10 rounded-full" />
+                <img src={Ryans} alt={shop} className="w-20" />
               )}
               {shop === "PC House" && (
-                <img
-                  src={PCHouse}
-                  alt={shop}
-                  className="size-10 rounded-full"
-                />
+                <img src={PCHouse} alt={shop} className="w-20" />
               )}
-              {shop === "'TechLand'" && (
-                <img
-                  src={TechLand}
-                  alt={shop}
-                  className="size-10 rounded-full"
-                />
+              {shop === "TechLand" && (
+                <img src={TechLand} alt={shop} className="w-20 bg-black p-3" />
+              )}
+              {shop === "Ultra Technology" && (
+                <img src={Ultra} alt={shop} className="w-20" />
               )}
               <h2 className="text-xl mb-4">{shop}</h2>
               <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
--- Server/server.js
+++ Server/server.js
@@ -1,3 +1,333 @@
+// const express = require("express");
+// const puppeteer = require("puppeteer");
+
+// const cors = require("cors");
+// const app = express();
+// const PORT = 3000;
+// app.use(cors());
+
+// const startScraping = async (product) => {
+//   const results = {};
+
+//   const browser = await puppeteer.launch({
+//     headless: false,
+//     defaultViewport: null,
+//     args: ["--start-maximized", "--no-sandbox", "--disable-setuid-sandbox"],
+//   });
+
+//   const scrapeStarTech = async () => {
+//     const page = await browser.newPage();
+//     await page.goto("https://www.startech.com.bd/");
+
+//     try {
+//       await page.waitForSelector("#search input[name=search]", {
+//         visible: true,
+//       });
+//       await page.type("#search input[name=search]", product);
+
+//       await page.waitForSelector("#search button", { visible: true });
+//       await page.click("#search button");
+
+//       await page.waitForSelector(".main-content", { visible: true });
+
+//       const products = await page.evaluate(() => {
+//         const items = Array.from(document.querySelectorAll(".p-item"));
+//         return items.map((item) => {
+//           const name = item.querySelector(".p-item-name")
+//             ? item.querySelector(".p-item-name").innerText
+//             : "Name not found";
+//           const price = item.querySelector(".p-item-price")
+//             ? item.querySelector(".p-item-price").innerText
+//             : "Out Of Stock";
+//           const img = item.querySelector(".p-item-img img")
+//             ? item.querySelector(".p-item-img img").src
+//             : "Image not found";
+//           const link = item.querySelector(".p-item-img a")
+//             ? item.querySelector(".p-item-img a").href
+//             : "Link not found";
+//           return { name, price, img, link };
+//         });
+//       });
+
+//       results["StarTech"] = products;
+//     } catch (error) {
+//       console.error("Error scraping StarTech:", error);
+//     }
+//   };
+
+//   const scrapeTechLand = async () => {
+//     const page = await browser.newPage();
+//     await page.goto("https://www.techlandbd.com/");
+
+//     try {
+//       const popupButton = await page.$(".popup-container button");
+//       if (popupButton) {
+//         await popupButton.click();
+//         console.log("Popup closed.");
+//       } else {
+//         console.log("No popup found.");
+//       }
+
+//       await page.waitForSelector(".header-search input[name=search]", {
+//         visible: true,
+//       });
+//       await page.type(".header-search input[name=search]", product);
+
+//       await page.waitForSelector(".header-search button", {
+//         visible: true,
+//       });
+//       await page.evaluate(() => {
+//         document.querySelector(".header-search button").click();
+//       });
+
+//       await page.waitForSelector(".main-products-wrapper", {
+//         visible: true,
+//       });
+
+//       const products = await page.evaluate(() => {
+//         const items = Array.from(document.querySelectorAll(".product-layout"));
+//         return items.map((item) => {
+//           const name = item.querySelector(".name")
+//             ? item.querySelector(".name").innerText
+//             : "Name not found";
+//           const price = item.querySelector(".price-new")
+//             ? item.querySelector(".price-new").innerText
+//             : "Out Of Stock";
+//           const img = item.querySelector(".image img")
+//             ? item.querySelector(".image img").src
+//             : "Image not found";
+//           const link = item.querySelector(".product-img")
+//             ? item.querySelector(".product-img").href
+//             : "Link not found";
+//           return { name, price, img, link };
+//         });
+//       });
+
+//       results["TechLand"] = products;
+//     } catch (error) {
+//       console.error("Error scraping TechLand:", error);
+//     }
+//   };
+
+//   const scrapeRyans = async () => {
+//     const page = await browser.newPage();
+//     await page.goto("https://www.ryans.com/");
+
+//     try {
+//       await page.waitForSelector(".search-input", {
+//         visible: true,
+//       });
+//       await page.type(".search-input", product);
+
+//       await page.waitForSelector(".search-btn", { visible: true });
+//       await page.click(".search-btn");
+
+//       await page.waitForSelector(".recent-view-section", { visible: true });
+
+//       const products = await page.evaluate(() => {
+//         const items = Array.from(
+//           document.querySelectorAll(".category-single-product")
+//         );
+//         return items.map((item) => {
+//           const name = item.querySelector(".p-item-name")
+//             ? item.querySelector(".p-item-name").innerText
+//             : "Name not found";
+//           const price = item.querySelector(".pr-text")
+//             ? item.querySelector(".pr-text").innerText
+//             : "Out Of Stock";
+//           const img = item.querySelector(".image-box img")
+//             ? item.querySelector(".image-box img").src
+//             : "Image not found";
+//           const link = item.querySelector(".image-box a")
+//             ? item.querySelector(".image-box a").href
+//             : "Link not found";
+//           return { name, price, img, link };
+//         });
+//       });
+
+//       results["Ryans"] = products;
+//     } catch (error) {
+//       console.error("Error scraping Ryans:", error);
+//     }
+//   };
+
+//   const scrapeBinary = async () => {
+//     const page = await browser.newPage();
+//     await page.goto("https://www.binarylogic.com.bd/");
+
+//     try {
+//       await page.waitForSelector("#searchInput", {
+//         visible: true,
+//       });
+//       await page.type("#searchInput", product);
+
+//       await page.waitForSelector(".new_m_searchbox button", { visible: true });
+//       await page.click(".new_m_searchbox button");
+
+//       await page.waitForSelector(".product_column", { visible: true });
+
+//       const products = await page.evaluate(() => {
+//         const items = Array.from(document.querySelectorAll(".single_product"));
+//         return items.map((item) => {
+//           const name = item.querySelector(".p-item-name")
+//             ? item.querySelector(".p-item-name").innerText
+//             : "Name not found";
+//           const price = item.querySelector(".current_price")
+//             ? item.querySelector(".current_price").innerText
+//             : "Out Of Stock";
+//           const img = item.querySelector(".p-item-img img")
+//             ? item.querySelector(".p-item-img img").src
+//             : "Image not found";
+//           const link = item.querySelector(".p-item-img a")
+//             ? item.querySelector(".p-item-img a").href
+//             : "Link not found";
+//           return { price, img, link };
+//         });
+//       });
+
+//       results["Binary"] = products;
+//     } catch (error) {
+//       console.error("Error scraping Binary:", error);
+//     }
+//   };
+
+//   const scrapeUltraTech = async () => {
+//     const page = await browser.newPage();
+//     await page.goto("https://www.ultratech.com.bd/");
+
+//     try {
+//       const popupButton = await page.$(".popup-container button");
+//       if (popupButton) {
+//         await popupButton.click();
+//         console.log("Popup closed.");
+//       } else {
+//         console.log("No popup found.");
+//       }
+
+//       await page.waitForSelector(".header-search input[name=search]", {
+//         visible: true,
+//       });
+//       await page.type(".header-search input[name=search]", product);
+
+//       await page.waitForSelector(".header-search button", { visible: true });
+//       await page.click(".header-search button");
+
+//       await page.waitForSelector(".main-products-wrapper", {
+//         visible: true,
+//       });
+
+//       const products = await page.evaluate(() => {
+//         const items = Array.from(document.querySelectorAll(".product-layout"));
+//         return items.map((item) => {
+//           const name = item.querySelector(".name")
+//             ? item.querySelector(".name").innerText
+//             : "Name not found";
+//           const price = item.querySelector(".price-new")
+//             ? item.querySelector(".price-new").innerText
+//             : "Out Of Stock";
+//           const img = item.querySelector(".image img")
+//             ? item.querySelector(".image img").src
+//             : "Image not found";
+//           const link = item.querySelector(".product-img")
+//             ? item.querySelector(".product-img").href
+//             : "Link not found";
+//           return { name, price, img, link };
+//         });
+//       });
+
+//       results["Ultra Technology"] = products;
+//     } catch (error) {
+//       console.error("Error scraping Ultra Technology", error);
+//     }
+//   };
+
+//   const scrapePcHouse = async () => {
+//     const page = await browser.newPage();
+//     await page.goto("https://www.pchouse.com.bd/");
+
+//     try {
+//       const popupButton = await page.$(".popup-container button");
+//       if (popupButton) {
+//         await popupButton.click();
+//         console.log("Popup closed.");
+//       } else {
+//         console.log("No popup found.");
+//       }
+
+//       await page.waitForSelector(".header-search input[name=search]", {
+//         visible: true,
+//       });
+//       await page.type(".header-search input[name=search]", product);
+
+//       await page.waitForSelector(".header-search button", { visible: true });
+//       await page.click(".header-search button");
+
+//       await page.waitForSelector(".main-products-wrapper", {
+//         visible: true,
+//       });
+
+//       const products = await page.evaluate(() => {
+//         const items = Array.from(document.querySelectorAll(".product-layout"));
+//         return items.map((item) => {
+//           const name = item.querySelector(".name")
+//             ? item.querySelector(".name").innerText
+//             : "Name not found";
+//           const price = item.querySelector(".price-new")
+//             ? item.querySelector(".price-new").innerText
+//             : "Out Of Stock";
+//           const img = item.querySelector(".image img")
+//             ? item.querySelector(".image img").src
+//             : "Image not found";
+//           const link = item.querySelector(".product-img")
+//             ? item.querySelector(".product-img").href
+//             : "Link not found";
+//           return { name, price, img, link };
+//         });
+//       });
+
+//       results["PC House"] = products;
+//     } catch (error) {
+//       console.error("Error scraping PC House", error);
+//     }
+//   };
+
+//   await Promise.all([
+//     scrapeStarTech(),
+//     scrapeTechLand(),
+//     scrapeRyans(),
+//     scrapeBinary(),
+//     scrapeUltraTech(),
+//     scrapePcHouse(),
+//   ]);
+
+//   await browser.close();
+//   return results;
+// };
+
+// app.get("/", (res) => {
+//   res.send("Welcome to the Price Scraper API!");
+// });
+
+// app.get("/scrape", async (req, res) => {
+//   const { product } = req.query;
+//   if (!product) {
+//     return res
+//       .status(400)
+//       .send({ error: "Product query parameter is required" });
+//   }
+
+//   try {
+//     const results = await startScraping(product);
+//     res.json(results);
+//   } catch (error) {
+//     res.status(500).send({ error: "Error during scraping" });
+//   }
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
 const express = require("express");
 const puppeteer = require("puppeteer");
 
@@ -10,9 +340,9 @@ const startScraping = async (product) => {
   const results = {};
 
   const browser = await puppeteer.launch({
-    headless: true,
+    headless: false,
     defaultViewport: null,
-    args: ["--start-maximized"],
+    args: ["--start-maximized", "--no-sandbox", "--disable-setuid-sandbox"],
   });
 
   const scrapeStarTech = async (page) => {
@@ -119,7 +449,7 @@ const startScraping = async (product) => {
       await page.waitForSelector(".search-btn", { visible: true });
       await page.click(".search-btn");
 
-      await page.waitForSelector(".recent-view-section", { visible: true });
+      await page.waitForSelector("#search-box-html", { visible: true });
 
       const products = await page.evaluate(() => {
         const items = Array.from(
@@ -287,11 +617,11 @@ const startScraping = async (product) => {
 
   const page = await browser.newPage();
   await scrapeStarTech(page);
-  await scrapeRyans(page);
-  await scrapeBinary(page);
-  await scrapeUltraTech(page);
-  await scrapePcHouse(page);
   await scrapeTechLand(page);
+  await scrapeRyans(page);
+  await scrapePcHouse(page);
+  await scrapeUltraTech(page);
+  await scrapeBinary(page);
 
   await browser.close();
   return results;
