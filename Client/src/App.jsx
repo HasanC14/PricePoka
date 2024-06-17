4e66682b95ce1f929cff296d8f8528f19f1d1ed5
HasanC14 <chowdhuryh999@gmail.com>
2024-06-17 12:51:21 +0600

    page visit counter added

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,35 @@
 import ProductSearch from "./components/ProductSearch";
 import "./App.css";
 import Footer from "./components/Footer";
+import countapi from "countapi-js";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 function App() {
+  const [visitors, setVisitors] = useState(null);
+  const namespace = "pricechecker";
+  const key = "hasan14";
+
+  useEffect(() => {
+    const fetchVisitors = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.countapi.xyz/hit/${namespace}/${key}`
+        );
+        console.log(response.data);
+        setVisitors(response.data.value);
+      } catch (error) {
+        console.error("Error fetching the data", error);
+      }
+    };
+
+    fetchVisitors();
+  }, []);
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen max-w-3xl mx-auto my-4">
+    <div
+      id="visits"
+      className="flex flex-col items-center justify-center min-h-screen max-w-3xl mx-auto my-4"
+    >
       {/* <header className="">
         <h1 className="text-3xl gradient-text font-semibold">Price Checker</h1>
         <div className="text-xs text-prime">
@@ -16,6 +42,9 @@ function App() {
       <footer>
         <Footer />
       </footer>
+      <div>
+        <h1>Total Visitors: {visitors !== null ? visitors : "Loading..."}</h1>
+      </div>
     </div>
   );
 }
