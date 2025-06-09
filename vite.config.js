e33f1ebe6fc632d5527b6966e7aaceefa5b1fc19
HasanC14 <dev.hasanchowdhury@gmail.com>
2025-06-09 09:03:15 +0600

    logo not showing on root folder fixed

--- vite.config.js
+++ vite.config.js
@@ -16,13 +16,13 @@ export default defineConfig({
         theme_color: "#ffffff",
         icons: [
           {
-            src: "logo-s.png",
+            src: "/logo-s.png",
             // src: 'pwa-192x192.png',
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "logo-l.jpg",
+            src: "/logo-l.jpg",
             // src: 'pwa-512x512.png',
             sizes: "512x512",
             type: "image/png",
